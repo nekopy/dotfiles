d6f9492359512512440d2b994a3be56fb432d2cc
nekopy <nekopy@protonmail.com>
2026-02-25 15:27:55 -0800

    Use Neogit

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -226,6 +226,21 @@ require("lazy").setup({
 			})
 		end,
 	},
+	{
+		"NeogitOrg/neogit",
+		lazy = true,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+
+			"sindrets/diffview.nvim",
+			-- "esmuellert/codediff.nvim" -- other option for diff
+			"nvim-telescope/telescope.nvim",
+		},
+		cmd = "Neogit",
+		keys = {
+			{ "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+		},
+	},
 
 	-- NOTE: Plugins can specify dependencies.
 	--
